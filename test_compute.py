df74d487e72be6583cb6dadbe490d7970289ad8b
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-02-16 16:40:36 +0100

    iter

--- test_compute.py
+++ test_compute.py
@@ -1,6 +1,12 @@
 from compute import divide
+from compute import multiply
 
 
 def test_divide():
     x = divide(1, 2)
     assert x == 0.5
+
+
+def test_multiply():
+    x = multiply(2, 2)
+    assert x == 4
