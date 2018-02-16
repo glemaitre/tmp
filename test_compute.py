228e17e2ab8897c52df8f73be46641c15e6db8f8
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-02-16 16:53:26 +0100

    iter

--- test_compute.py
+++ test_compute.py
@@ -1,10 +1,18 @@
+import pytest
+import numpy as np
+
 from compute import divide
 from compute import multiply
 
 
-def test_divide():
-    x = divide(1, 2)
-    assert x == 0.5
+@pytest.mark.parametrize(
+    "lop, rop, res",
+    [(1, 2, 0.5),
+     (1, 0, np.inf)]
+)
+def test_divide(lop, rop, res):
+    x = divide(lop, rop)
+    assert x == pytest.approx(res)
 
 
 def test_multiply():
